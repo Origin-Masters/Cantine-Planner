42517bd79aab279f0d5291443aa4c98de08f217a
Michał Roziel <rozielmichal@gmail.com>
2025-01-23 17:31:42 +0100

    created meal type mapper in businessLogic

---
+++ cantine-planner/src/main/java/de/htwsaar/cantineplanner/businessLogic/MealTypeMapper.java
@@ -0,0 +1,23 @@
+package de.htwsaar.cantineplanner.businessLogic;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MealTypeMapper {
+
+    private static final Map<Integer, String> mealTypeMap = new HashMap<>();
+
+    static {
+        mealTypeMap.put(0, "Meat");
+        mealTypeMap.put(1, "Vegetarian");
+        mealTypeMap.put(2, "Vegan");
+        /*
+        mealTypeMap.put(4, "Fish");
+        mealTypeMap.put(5, "Dessert");
+        */
+    }
+
+    public static String getMealTypeName(int id) {
+        return mealTypeMap.getOrDefault(id, "Unknown Meal Type");
+    }
+}
